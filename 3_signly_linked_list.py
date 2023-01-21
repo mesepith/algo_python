13af9a06c3232e798cde8cc54cfe004ad44179cb
mesepith <zahiralam29@gmail.com>
2023-01-21 15:50:20 +0530

    singly linked list : add node at the begging

--- 3_signly_linked_list.py
+++ 3_signly_linked_list.py
@@ -22,19 +22,51 @@ class Node:
     def __repr__(self):
         return "<Node data: %s>" %self.dataz1;
 
+class LinkedList:
 
-"Call Node class with instance N1 "
-""" Nodes are building blocks of list(python) """
-N1 = Node(10);
-print("Value of N1 is : ")
-print(N1)
+    """
+    Singly linked list
+    """
 
-"Call 2nd node N2"
-N2 = Node(20);
+    "create instance variable to store head node of the list"
+    head = None;
 
-"Set next node of N1 which is N2"
-N1.next_node=N2;
+    "create constructor to create list"
+    def __init__(self):
+        self.head = None;
 
-print(N1.next_node)
+
+    "create function to add node at the beginning of the list"
+    def add_node_at_beginning(self,dataz4):
+        print()
+        "create new"
+        new_node = Node(dataz4);
+        "check if list is empty"
+        print('new_node ', new_node)
+        print('self.head ', self.head)
+        if self.head is None:
+            self.head = new_node;
+            print(' self.head or new_node ', new_node)
+            return;
+        "if list is not empty"
+        print('self.head : if list is not empty', self.head)
+        new_node.next_node = self.head;
+        self.head = new_node;
+
+
+print();
+
+ #call add_node_at_beginning function
+L1 = LinkedList();
+L1.add_node_at_beginning(10);
+L1.add_node_at_beginning(20);
+L1.add_node_at_beginning(30);
+L1.add_node_at_beginning(40);
+L1.add_node_at_beginning(50);
+print()
+print("Value of L1 which is calling add_node_at_beginning fun is : ")
+print(L1)
+
+print();
 
 
