72f15213535ee780c180a6973e4a3340f7b87155
mesepith <zahiralam29@gmail.com>
2023-01-23 17:38:45 +0530

    add_node_at_end function added

--- 3_signly_linked_list.py
+++ 3_signly_linked_list.py
@@ -66,15 +66,29 @@ class LinkedList:
         new_node.next_node = self.head;
         self.head = new_node;
 
+    "create function to add node at the end of the list"
+    def add_node_at_end(self,dataz5):
+        "create new"
+        new_node = Node(dataz5);
+        "check if list is empty"
+        if self.head is None:
+            self.head = new_node;
+            return;
+        "if list is not empty"
+        current_node = self.head;
+        while current_node.next_node:
+            current_node = current_node.next_node;
+        current_node.next_node = new_node;
+
 print();
 
  #call add_node_at_beginning function
 L1 = LinkedList();
 L1.add_node_at_beginning(10);
 L1.add_node_at_beginning(20);
-# L1.add_node_at_beginning(30);
-# L1.add_node_at_beginning(40);
-# L1.add_node_at_beginning(50);
+L1.add_node_at_beginning(30);
+L1.add_node_at_beginning(40);
+L1.add_node_at_beginning(50);
 print()
 print("Value of L1 which is calling add_node_at_beginning fun is : ")
 print(L1)
@@ -82,12 +96,12 @@ print(L1)
 print();
 
 #call add_node_at_end function
-# L2 = LinkedList();
-# L2.add_node_at_end(10);
-# L2.add_node_at_end(20);
-# L2.add_node_at_end(30);
-# L2.add_node_at_end(40);
-# L2.add_node_at_end(50);
-# print("Value of L2 which is calling add_node_at_end fun is : ")
-# print(L2)
+L2 = LinkedList();
+L2.add_node_at_end(10);
+L2.add_node_at_end(20);
+L2.add_node_at_end(30);
+L2.add_node_at_end(40);
+L2.add_node_at_end(50);
+print("Value of L2 which is calling add_node_at_end fun is : ")
+print(L2)
 
